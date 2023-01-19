519ccfa164d9b9332f38bc0564f1c1065761a55e
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-01-18 23:14:54 -0500

    Initial commit

---
+++ Project_1/calculator.cpp
@@ -0,0 +1,31 @@
+// Libraries
+#include <iostream>
+#include <string>
+
+int main();
+
+class Node {
+    private: 
+        // Stores the value of Node
+        double val;
+        // Stores the name of Node
+        std::string s;
+        // Stores the next Node
+        Node* next;
+};
+
+// Class for calculator-based linked list
+class Calculator
+{
+    // Private variables
+    private:
+        // Size stores the size of array
+        int size;
+        // Stores the head of the linked list
+        Node* head;
+};
+
+int main()
+{
+
+}
